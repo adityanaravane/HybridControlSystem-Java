294f5d80a1ac130bcc41b0c1a7113d286e879995
Aditya Naravane <adityanaravane2008@gmail.com>
2024-04-11 17:45:37 +0000

    variable size vector work

--- lib/src/main/java/VariableSizeVector.java
+++ lib/src/main/java/VariableSizeVector.java
@@ -1,25 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.logging.ErrorManager;
 
 public class VariableSizeVector {
-    private struct vector;
+    private ArrayList<Double> vector;
+    private ArrayList<String> indexes;
 
-    public VariableSizeVector(int size) {
-        this.vector = new double[] {};
-        for (i=0; i<=size; i++) {
-            this.vector = new double[]{this.vector.clone(), 0};
+    public VariableSizeVector(int size, String[] indexNames) {
+        if (indexNames.length != size) {
+            throw new Exception("Amount of IndexNames does not match array size");
         }
+        this.vector = new ArrayList<Double>();
+        this.indexes = new ArrayList<String>();
+        for(i=0; i<=size; i++) {
+            this.vector.add(0);
+            this.indexes.add(indexNames[i]);
+        }
+        
+
     }
 
-    public double[] get(){
+    public ArrayList<Double> get(){
         return this.vector;
     }
 
-    public void setValue(int index, double value) {
-        if (this.vector.length < index) {
+    public void setValues(int index, double[] value) {
+        if (this.vector.size() < index) {
             throw new Exception("Trying to add a number out of scope");
         }
+        if (this.vector.size() != value.length) {
+            throw new Exception("Not Enough Values");
+        }
 
-        this.vector[whereToPut] = value;
+
+
     }
 
 
